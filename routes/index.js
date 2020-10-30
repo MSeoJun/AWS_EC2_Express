0a79fd80cc3c19db04a63a28f100310184f79107
woobinslave <mun3582w@naver.com>
2020-10-30 10:54:50 +0900

    급식 조회

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
+const { default: Axios } = require('axios');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let a = [];
+  Axios.get('https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=e01aaa6932e5403ea3645b35e1c0a12d&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530336&MLSV_YMD=20201029')
+  .then(res => {
+    const test = res.data.mealServiceDietInfo[1].row[0].DDISH_NM;
+    for(let i=0; i<test.split("<br/>").length; i++) {
+      a[i] = test.split("<br/>")[i].match(/[^(삼일)]/g).join('').match(/[가-힣]/g).join("");
+      console.log(a[i]);
+    }
+  })
+  .then(() => {
+    res.render('index', { title: 'Express' ,  data:a });
+  })
 });
 
 module.exports = router;
