5298b5a4787a615aa8e0f3ce27f4445ae66889bb
woobinslave <mun3582w@naver.com>
2020-10-30 16:45:42 +0900

    급식

--- routes/launch.js
+++ routes/launch.js
@@ -2,10 +2,14 @@ const { default: Axios } = require('axios');
 var express = require('express');
 var router = express.Router();
 
+require('date-utils');
+var newDate = new Date();
+var time = newDate.toFormat('YYYYMMDD');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let a = [];
-  Axios.get('https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=e01aaa6932e5403ea3645b35e1c0a12d&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530336&MLSV_YMD=20201030')
+  Axios.get(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=e01aaa6932e5403ea3645b35e1c0a12d&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530336&MLSV_YMD=${time}`)
   .then(res => {
     const test = res.data.mealServiceDietInfo[1].row[0].DDISH_NM;
     for(let i=0; i<test.split("<br/>").length; i++) {
